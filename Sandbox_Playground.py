df90c4f1ad961daa3052a39ba79f61cba1876639
BrianK66618 <kbrian0618@gmail.com>
2023-10-12 08:27:33 -0400

    add print lines introducing myself
    I added print lines to introduce myself and what im looking forward to

--- Sandbox_Playground.py
+++ Sandbox_Playground.py
@@ -2,3 +2,8 @@
 # Date: 10.11.2023
 # Program: Sandbox Playground
 
+print("Hello, i'm Brian Kuhn, & the longest word in the dictionary is Pneumonoultramicroscopicsilicovolcanoconiosis!\n")
+print("I am looking forward to saying Pneumonoultramicroscopicsilicovolcanoconiosis, and a Halloween party!")
+import sys
+sys.exit()
+print("this text cannot be seen by playing this code")
