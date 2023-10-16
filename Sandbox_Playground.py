12b16a5450c6d3d1f93c4b13e42fbf998f007eca
BrianK66618 <kbrian0618@gmail.com>
2023-10-16 08:11:37 -0400

    Added a Print Statement
    Why IS Monday Called Monday?

--- Sandbox_Playground.py
+++ Sandbox_Playground.py
@@ -6,5 +6,6 @@ print("Hello, i'm Brian Kuhn, & the longest word in the dictionary is Pneumonoul
 print("I am looking forward to saying Pneumonoultramicroscopicsilicovolcanoconiosis, and a Halloween party!")
 print("I Have Absolutely No Clue Why I'm Doing This")
 import sys
+print("Why did we name Monday Monday?")
 sys.exit()
 print("this text cannot be seen by playing this code")
